89abe3ccc3ad5e829f7aec6c585d9cde0b1939f2
nduka-vic <vic.nduka@gmail.com>
2025-09-29 18:07:36 +0100

    implement initial HeaderBox and TotalBalanceBox, with Doughnut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const isLoggedIn = { firstName: "Victorlight" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={isLoggedIn?.firstName || "Guest"}
+            subtext="Access account and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
