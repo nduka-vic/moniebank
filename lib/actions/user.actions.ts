179d0c3577d6b96ad889a14a55b312cd269c8563
nduka-vic <vic.nduka@gmail.com>
2025-10-16 22:24:37 +0100

    implement initial sign up authentication with appwrite

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const signIn = async () => {
+  try {
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
+
+  try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
+  } catch (error: any) {
+    console.error("5. FULL Error in signUp:", {
+      message: error.message,
+      code: error.code,
+      type: error.type,
+      stack: error.stack,
+    });
+    return null;
+  }
+};
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
+}
