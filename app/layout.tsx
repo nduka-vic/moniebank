d46df42e19ea91b8581afa3621e41ca907e5e04b
nduka-vic <vic.nduka@gmail.com>
2025-10-24 15:37:38 +0100

    implement sentry connections

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,18 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
+// import * as Sentry from "@sentry/nextjs";
+
+// // Add or edit your "generateMetadata" to include the Sentry trace data:
+// export function generateMetadata(): Metadata {
+//   return {
+//     // ... your existing metadata
+//     other: {
+//       ...Sentry.getTraceData(),
+//     },
+//   };
+// }
+
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 const ibmPlexSerif = IBM_Plex_Serif({
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a85291aceb7d9381084583fc6ba88454@o4510244557094912.ingest.de.sentry.io/4510244591239248",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
